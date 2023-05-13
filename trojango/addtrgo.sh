083a89a0ddea31ecc48505e461318ea1a7d37e1e
NetworkStore <133486943+NetworkStore@users.noreply.github.com>
2023-05-13 23:41:39 +0700

    Update addtrgo.sh

--- trojango/addtrgo.sh
+++ trojango/addtrgo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SL
+# Andre Sakti
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -63,4 +63,4 @@ echo -e "Expired    : $exp"
 echo -e "========================="
 echo -e "Link TrGo  : ${link}"
 echo -e "========================="
-echo -e "Script Mod By SL"
+echo -e "Script Mod By Andre Sakti"
