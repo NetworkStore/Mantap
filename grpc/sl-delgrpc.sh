fbd16f1402ed7de05022d072bde4d27c872f0a30
NetworkStore <133486943+NetworkStore@users.noreply.github.com>
2023-05-13 22:11:05 +0700

    Update sl-delgrpc.sh

--- grpc/sl-delgrpc.sh
+++ grpc/sl-delgrpc.sh
@@ -1,7 +1,7 @@
 # ==========================================
 # Getting
 #!/bin/bash
-# SL
+# Andre Sakti
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -71,4 +71,4 @@ echo "==============================="
 echo "Username  : $user"
 echo "Expired   : $exp"
 echo "==============================="
-echo "Script Mod By SL"
+echo "Script Mod By Andre Sakti"
