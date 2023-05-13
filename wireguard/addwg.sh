f285cd4bfb53f0acf3aa8f70d944cd7d14bb6462
NetworkStore <133486943+NetworkStore@users.noreply.github.com>
2023-05-13 23:54:07 +0700

    Update addwg.sh

--- wireguard/addwg.sh
+++ wireguard/addwg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SL
+# Andre Sakti
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -113,5 +113,5 @@ echo -e "Expired  : $exp"
 echo -e "======================="
 echo -e "Link WG  : http://$MYIP:89/$CLIENT_NAME.conf"
 echo -e "======================="
-echo -e "Script Mod By SL"
+echo -e "Script Mod By Andre Sakti"
 rm -f /root/wg0-client-$CLIENT_NAME.conf
