74f10ee9bc34741db21b6d096c3128a04df84d7a
NetworkStore <133486943+NetworkStore@users.noreply.github.com>
2023-05-16 10:12:09 +0700

    Update install-bandwidth.sh

--- cek-bandwidth/install-bandwidth.sh
+++ cek-bandwidth/install-bandwidth.sh
@@ -2,9 +2,9 @@
 
 cd /usr/bin
 
-rm installbandwidth
+rm cek-bandwidth
 
-wget -O info-cek-tersedia "https://raw.githubusercontent.com/NetworkStore/Mantap/main/cek-bandwidth/installbandwidth.sh"
+wget -O info-cek-tersedia "https://raw.githubusercontent.com/NetworkStore/Mantap/main/cek-bandwidth/cek-bandwidth.sh"
 
-chmod +x installbandwidth
+chmod +x cek-bandwidth
 
