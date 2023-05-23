a6b5a32662be74cbc39e358a075a8dfc4fc49aec
NetworkStore <133486943+NetworkStore@users.noreply.github.com>
2023-05-23 22:35:44 +0700

    Update add-ssws.sh

--- vmess-ws/add-ssws.sh
+++ vmess-ws/add-ssws.sh
@@ -51,8 +51,8 @@ export WARNING="${RED}\e[5m"
 export UNDERLINE="\e[4m"
 
 # // Exporting URL Host
-export Server_URL="raw.githubusercontent.com/andre-sakti/test/main"
-export Server1_URL="raw.githubusercontent.com/andre-sakti/limit/main"
+export Server_URL="raw.githubusercontent.com/NetworkStore/test/main"
+export Server1_URL="raw.githubusercontent.com/NetworkStore/limit/main"
 export Server_Port="443"
 export Server_IP="underfined"
 export Script_Mode="Stable"
